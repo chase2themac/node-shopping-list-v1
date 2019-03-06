722cf2aadf1739ae5ccf40b4c35b7f42dbeeec3a
James Tyler <macndchase@gmail.com>
2019-03-06 08:07:26 -0500

    added recipes req

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const bodyParser = require('body-parser');
 // interact with in our GET endpoint
 const {ShoppingList} = require('./models');
 
+const {Recipes} = require('./models');
+
 const jsonParser = bodyParser.json();
 const app = express();
 
@@ -24,11 +26,16 @@ app.use(morgan('common'));
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
-
+Recipes.create('chocolate milk', ['chocolate syrup', 'milk']);
+Recipes.create('Hot Cocoa', ['cocoa mix', 'hot water','marshmellows(optional)' ]);
 // when the root of this route is called with GET, return
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req,res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
